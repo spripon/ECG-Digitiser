4c80f3f6b1ca3623bda324250c168848d725516e
felixkrones <info@felixkrones.de>
2024-08-10 17:13:53 +0100

    Train but test M1

--- team_code.py
+++ team_code.py
@@ -214,8 +214,10 @@ SIGNAL_START = {
 
 # nnUNet settings
 NNUNET_RAW = f"{os.getcwd()}/data/ptb-xl"
-NNUNET_PREPROCESSED = f"{os.getcwd()}/model/nnUNet_preprocessed"
-NNUNET_RESULTS = f"{os.getcwd()}/model/nnUNet_results"
+NNUNET_PREPROCESSED_TRAIN = f"{os.getcwd()}/model/nnUNet_preprocessed"
+NNUNET_RESULTS_TRAIN = f"{os.getcwd()}/model/nnUNet_results"
+NNUNET_PREPROCESSED_USE = f"{os.getcwd()}/model/M1/nnUNet_preprocessed"
+NNUNET_RESULTS_USE = f"{os.getcwd()}/model/M1/nnUNet_results"
 
 # TODO: Train on float rotated images
 # TODO: Lead boxes: Do we need separate models for lead and lead name? Should we use one box per line?
@@ -376,8 +378,8 @@ def train_models(data_folder, model_folder, verbose):
         if verbose:
             print('--------- Training nnUNet model... ---------')
         os.environ["nnUNet_raw"] = data_folder
-        os.environ["nnUNet_preprocessed"] = NNUNET_PREPROCESSED
-        os.environ["nnUNet_results"] = NNUNET_RESULTS
+        os.environ["nnUNet_preprocessed"] = NNUNET_PREPROCESSED_TRAIN
+        os.environ["nnUNet_results"] = NNUNET_RESULTS_TRAIN
         command_run_preprocess = "nnUNetv2_plan_and_preprocess -d 500 --clean -c 2d --verify_dataset_integrity"
         command_run_train = "nnUNetv2_train 500 2d all -device cuda --c"
         subprocess.run(command_run_preprocess, shell=True)
@@ -1524,7 +1526,7 @@ def predict_mask_nnunet(image, dataset_name):
     #mask_path_temp = os.path.join(temp_folder_output_pp, "00000_temp.png")
 
     # Define run commands
-    command_run = f"nnUNetv2_predict -d {dataset_name} -i {temp_folder_input} -o {temp_folder_output} -f  all -tr nnUNetTrainer -c 2d -p nnUNetPlans"
+    command_run = f"nnUNetv2_predict -d {dataset_name} -i {temp_folder_input} -o {temp_folder_output} -f 0 -tr nnUNetTrainer -c 2d -p nnUNetPlans"
     command_post_process = f"nnUNetv2_apply_postprocessing -i {temp_folder_output} -o {temp_folder_output_pp} -pp_pkl_file model/nnUNet_results/{dataset_name}/nnUNetTrainer__nnUNetPlans__2d/crossval_results_folds_0/postprocessing.pkl -np 8 -plans_json model/nnUNet_results/{dataset_name}/nnUNetTrainer__nnUNetPlans__2d/crossval_results_folds_0/plans.json"
 
     # Create temp folders:
@@ -1534,8 +1536,8 @@ def predict_mask_nnunet(image, dataset_name):
 
     # Set env variabels (nnUNet needs them to be set)
     os.environ["nnUNet_raw"] = NNUNET_RAW
-    os.environ["nnUNet_preprocessed"] = NNUNET_PREPROCESSED
-    os.environ["nnUNet_results"] = NNUNET_RESULTS
+    os.environ["nnUNet_preprocessed"] = NNUNET_PREPROCESSED_USE
+    os.environ["nnUNet_results"] = NNUNET_RESULTS_USE
 
     # Save image
     write_png(image, image_path_temp)
